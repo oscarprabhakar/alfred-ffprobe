813bfd24cfe1771b99b2ad0b1168a856ffd61727
Oscar Prabhakar <oscar.prabhakar@joyn.de>
2022-07-22 17:35:57 +0200

    Move executor function

--- index.ts
+++ index.ts
@@ -1,38 +1,7 @@
 import alfy from 'alfy';
 import { spawn } from 'child_process';
-import { AudioStream, Dependencies, Executor, ProcessError, Stream, StreamType } from './types';
-
-function createExecutor(dependencies: Dependencies): Executor {
-    const { spawn } = dependencies;
-
-    return {
-        async run(executablePath, args) {
-            return new Promise((resolve, reject) => {
-
-                const errorOutput: string[] = [];
-                const output: string[] = [];
-                const process = spawn(executablePath, args);
-
-                process.once('error', reject);
-                process.stdout.on('data', (data) => {
-                    output.push(data.toString('utf8'));
-                });
-                process.stderr.on('data', (data) => {
-                    errorOutput.push(data.toString('utf8'));
-                });
-                process.once('exit', (code) => {
-                    if (code !== 0) {
-                        const message = `Error in Process ${executablePath}. Exit code: ${code}`;
-                        const error = new ProcessError(message, errorOutput.join(''), output.join(''));
-                        reject(error);
-                    } else {
-                        resolve(output.join(''));
-                    }
-                });
-            });
-        }
-    };
-}
+import { AudioStream, Stream, StreamType } from './types';
+import { createExecutor } from './process-executor';
 
 const runner = createExecutor({ spawn })
 const ffprobePath = '/usr/local/bin/ffprobe';
---
+++ process-executor.ts
@@ -0,0 +1,33 @@
+import { Dependencies, Executor, ProcessError } from "./types";
+
+export const createExecutor = (dependencies: Dependencies): Executor => {
+    const { spawn } = dependencies;
+
+    return {
+        async run(executablePath, args) {
+            return new Promise((resolve, reject) => {
+
+                const errorOutput: string[] = [];
+                const output: string[] = [];
+                const process = spawn(executablePath, args);
+
+                process.once('error', reject);
+                process.stdout.on('data', (data) => {
+                    output.push(data.toString('utf8'));
+                });
+                process.stderr.on('data', (data) => {
+                    errorOutput.push(data.toString('utf8'));
+                });
+                process.once('exit', (code) => {
+                    if (code !== 0) {
+                        const message = `Error in Process ${executablePath}. Exit code: ${code}`;
+                        const error = new ProcessError(message, errorOutput.join(''), output.join(''));
+                        reject(error);
+                    } else {
+                        resolve(output.join(''));
+                    }
+                });
+            });
+        }
+    };
+}
